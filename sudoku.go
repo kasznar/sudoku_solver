2747d5631cf6f5b01567feff7a0c1481af46c8af
kasznar <david@dotis.io>
2024-11-16 16:54:48 +0100

    check insert

--- sudoku.go
+++ sudoku.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Sudoku [][]int
 
 func (s Sudoku) print() {
@@ -25,42 +27,74 @@ func (s Sudoku) print() {
 	}
 }
 
+// todo: what if we already have value there?
+func (s Sudoku) isSafeToPlace(rowIndex int, colIndex int, value int) bool {
+	for i := 0; i < 9; i++ {
+		if s[rowIndex][i] == value {
+			return false
+		}
+	}
+
+	for i := 0; i < 9; i++ {
+		if s[i][colIndex] == value {
+			return false
+		}
+	}
+
+	blockStartRowIndex := rowIndex - rowIndex%3
+	blockStartColIndex := colIndex - colIndex%3
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if s[i+blockStartRowIndex][j+blockStartColIndex] == value {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
 var sudoku = Sudoku{
-	{0, 0, 0, 6, 0, 0, 0, 5, 4},
-	{0, 3, 0, 0, 0, 0, 0, 0, 0},
-	{6, 0, 0, 1, 8, 0, 0, 3, 9},
+	{3, 0, 6, 5, 0, 8, 4, 0, 0},
+	{5, 2, 0, 0, 0, 0, 0, 0, 0},
+	{0, 8, 7, 0, 0, 0, 0, 3, 1},
+	{0, 0, 3, 0, 1, 0, 0, 8, 0},
+	{9, 0, 0, 8, 6, 3, 0, 0, 5},
+	{0, 5, 0, 0, 9, 0, 6, 0, 0},
+	{1, 3, 0, 0, 0, 0, 2, 5, 0},
+	{0, 0, 0, 0, 0, 0, 0, 7, 4},
+	{0, 0, 5, 2, 0, 6, 3, 0, 0},
+}
 
-	{3, 0, 0, 0, 0, 2, 0, 9, 0},
-	{0, 9, 0, 0, 7, 0, 1, 0, 0},
-	{2, 0, 0, 0, 0, 0, 0, 0, 0},
+var solution = Sudoku{
+	{3, 1, 6, 5, 7, 8, 4, 9, 2},
+	{5, 2, 9, 1, 3, 4, 7, 6, 8},
+	{4, 8, 7, 6, 2, 9, 5, 3, 1},
+	{2, 6, 3, 4, 1, 5, 9, 8, 7},
+	{9, 7, 4, 8, 6, 3, 1, 2, 5},
+	{8, 5, 1, 7, 9, 2, 6, 4, 3},
+	{1, 3, 8, 9, 4, 7, 2, 5, 6},
+	{6, 9, 2, 3, 5, 1, 8, 7, 4},
+	{7, 4, 5, 2, 8, 6, 3, 1, 9},
+}
 
-	{0, 0, 4, 0, 0, 0, 0, 0, 0},
-	{0, 0, 7, 0, 6, 0, 0, 0, 8},
-	{8, 0, 0, 0, 0, 7, 2, 6, 0},
+func testSafetyCheck(rowIndex int, colIndex int, value int) {
+	print("It is ")
+
+	if sudoku.isSafeToPlace(rowIndex, colIndex, value) {
+		print("Safe")
+	} else {
+		print("Unsafe")
+	}
+
+	fmt.Printf(" to place %v at [%v][%v] \n", value, rowIndex, colIndex)
 }
 
 func main() {
 	sudoku.print()
-
+	testSafetyCheck(0, 1, 1)
+	testSafetyCheck(0, 1, 3)
+	testSafetyCheck(0, 1, 8)
 }
-
-/*package main
-
-import (
-	"fmt"
-	"math"
-)
-
-type Vector struct {
-	X, Y float64
-}
-
-func Abs(v Vector) float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-
-func main() {
-	v := Vector{3, 4}
-	fmt.Println(Abs(v))
-}
-*/
